341597492d3335fe5f97634f9917a8790ef23a96
bxlldev <127035655+bxlldev@users.noreply.github.com>
2023-11-17 16:43:50 +0700

    Arrays-Bankist update #3

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -83,13 +83,189 @@ const displayMovements = function (movements) {
 };
 
 displayMovements(account1.movements);
-
 // console.log(containerMovements.innerHTML);
+
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+
+calcDisplayBalance(account1.movements);
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner // add .username that stand for the fullname of each .owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createUsernames(accounts);
+
+/////////////////////////////////////////////////
+//-----------The Magic of Chaining Methods-------
+
+/*
+
+/////////////////////////////////////////////////
+//-----------Coding Challenge #2-------
+/*
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Your tasks:
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old,humanAge = 16 + dogAge * 4
+
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+
+4. Run the function for both test datasets
+
+Test data:
+§ Data 1: [5, 2, 4, 1, 15, 8, 3]
+§ Data 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+
+*/
+
+/*
+
+const calcAverageHumanAge = function (ages) {
+  // Using MAP Method
+  // const humanAge = ages.map((dogAge) => (dogAge <=2 ? 2*dogAge : 16 + dogAge*4)); // Another solution
+  const humanAge = ages.map(function (dogAge, i, arr) {
+    if (dogAge <= 2) return 2 * dogAge;
+    else if (dogAge > 2) return 16 + dogAge * 4;
+  });
+  console.log(`All Human Ages: ${humanAge}`);
+
+  // Using Filter Method
+  const excludeDogAge = humanAge.filter((age, i, arr) => age >= 18);
+  console.log(
+    `Exclude all dos less than 18 years old of Human: ${excludeDogAge}`
+  );
+
+  // Using Reduce Method
+  // const average = excludeDogAge.reduce((acc, cur) => acc + cur, 0)/ excludeDogAge.length; // Another Solution #1
+
+  // const average = excludeDogAge.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0); // Another Solution #2
+
+  // [2, 3] => (2+3)/2 = 2.5 === 2/2 + 3/2
+  const total = excludeDogAge.reduce((acc, cur) => {
+    return acc + cur;
+  }, 0);
+  console.log(`Total human's ages: ${total}`);
+  console.log(`Avarage human's ages: ${total / excludeDogAge.length}`);
+};
+
+console.log('------------Test Data #1------------');
+calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]); // Test data #1
+console.log('------------Test Data #2------------');
+calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]); // Test data #2
+
+*/
+
+/*
+
+///////////////////////////////////////////////
+//-----------Data Transformations: MAP, Filter, Reduce-------
+//-----------The Reduce Method (Difference callback func with MAP, Filter Method)-------
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+console.log(movements);
+
+// accumalator => SNOWBALL
+const balance1 = movements.reduce(function (acc, cur, i, arr) {
+  // console.log(`Iteration ${i}: ${acc}`);
+  return acc + cur;
+}, 0); // specify 0 in the end of callback function to start acc = 0 (if not specify, defalt acc also = 0);
+console.log(balance1);
+
+// Another way (Arrow function)
+const balance = movements.reduce((acc, cur) => acc + cur, 0);
+console.log(balance);
+
+// Another way (Forof loop)
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+
+// Maximum value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+console.log(max);
+
+*/
+
+/*
+/////////////////////////////////////////////////
+//-----------Data Transformations: MAP, Filter, Reduce-------
+//-----------The Filter Method-------
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const deposits = movements.filter(function (mov, i, arr) {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+
+// Another way (Forof loop)
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+// Challenge => filter only negative values (Arrow function call back)
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(movements);
+console.log(withdrawals);
+
+*/
+
+/*
+
+/////////////////////////////////////////////////
+//-----------Data Transformations: MAP, Filter, Reduce-------
+//-----------The MAP Method-------
+// Similar to Foreach Method********
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const eurToUsd = 1.1;
+const movementUSD = movements.map(function (mov) {
+  return mov * eurToUsd;
+});
+console.log(movements);
+console.log(movementUSD);
+
+// Another way (Try Forof loop)
+const movementUSDfor = [];
+for (const mov of movements) movementUSDfor.push(mov * eurToUsd);
+console.log(movementUSDfor);
+
+// Another way (Try with Arrow Function)
+const movementUSDarrow = movements.map(mov => mov * eurToUsd);
+console.log(movementUSDarrow);
+
+// Another way (Try Foreach loop)
+const movementDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+console.log(movementDescriptions);
+
+*/
+
+/*
 
 /////////////////////////////////////////////////
 // Coding Challenge #1
 
-/*
+
 
 Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy.
 A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
@@ -116,18 +292,43 @@ GOOD LUCK 😀
 
 */
 
-// let dogsJulia = [3, 5, 2, 12, 7];
-// dogsJulia.splice();
-// dogsJulia.splice(-2);
-// console.log(dogsJulia);
+/*
 
-// let dogsKate = [4, 1, 15, 8, 3];
+// My solution
+
+// Test data1
+const dogsJulia = [3, 5, 2, 12, 7];
+const dogsKate = [4, 1, 15, 8, 3];
+
+// Test data2
+// const dogsJulia = [9, 16, 6, 8, 3];
+// const dogsKate = [10, 5, 6, 1, 4];
+
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = [...dogsJulia]; // copy array
+  // const dogsJuliaCorrected = dogsJulia.slice(); // Another solution
+  dogsJuliaCorrected.splice(0, 1); // delete 1st element
+  dogsJuliaCorrected.splice(-2); // delete last two element
+  // dogsJuliaCorrected.slice(1, 3); // Another solution
+  console.log(dogsJuliaCorrected);
+
+  const dogs = dogsJuliaCorrected.concat(dogsKate);
+  console.log(dogs);
+
+  dogs.forEach(function (d, i, __) {
+    d >= 3
+      ? console.log(`Dog number ${i + 1} is an adult, and is ${d} years old `)
+      : console.log(`Dog number ${i + 1} is still a puppy🐶`);
+  });
+};
+
+checkDogs(dogsJulia, dogsKate);
+
+*/
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
-
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
