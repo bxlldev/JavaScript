cef1b707334c3af5dd366abe1db37d3f8394f5bf
bxlldev <127035655+bxlldev@users.noreply.github.com>
2023-11-22 11:29:22 +0700

    Arrays-Bankist update #7

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -293,7 +293,7 @@ const dogs = [
   { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
   { weight: 8, curFood: 200, owners: ['Matilda'] },
   { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
-  { weight: 23, curFood: 340, owners: ['Michael'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
 ];
 
 // console.log(dogs);
@@ -325,12 +325,59 @@ console.log(dogs);
 const sarahDog = dogs.find(own => own.owners.includes('Sarah'));
 console.log(sarahDog);
 console.log(
-  `Sarah's eating too ${
+  `Sarah's dog eating too ${
     sarahDog.curFood > sarahDog.recFood ? 'much' : 'little'
   }`
 );
 
 //-------3--------
+
+// const ownersEatTooMuch = dogs.filter(dog =>
+//   dog.curFood > dog.recFood
+//     ? console.log(`${dog.owners}'s dog eating too much`)
+//     : console.log(`${dog.owners}'s dog eating too little`)
+// );
+// console.log(ownersEatTooMuch);
+
+const ownersEatTooMuch = dogs
+  .filter(dog => dog.curFood > dog.recFood)
+  .flatMap(dog => dog.owners);
+console.log(ownersEatTooMuch);
+
+const ownersEatTooLittle = dogs
+  .filter(dog => dog.curFood < dog.recFood)
+  .flatMap(dog => dog.owners);
+console.log(ownersEatTooLittle);
+
+//-------4--------
+
+// `Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!`
+console.log(
+  `${ownersEatTooMuch.join(
+    ' and '
+  )} dogs eat too much! and ${ownersEatTooLittle.join(
+    ' and '
+  )} dogs eat too little!`
+);
+
+//-------5--------
+console.log(dogs.some(dog => dog.curFood === dog.recFood));
+
+//-------6--------
+//current > (recommended * 0.90) && current < (recommended * 1.10)
+
+const checkEatingOkay = dog =>
+  dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1;
+console.log(dogs.some(checkEatingOkay));
+
+//-------7--------
+console.log(dogs.filter(checkEatingOkay));
+
+//-------8--------
+// sort it by recommended food portion in an ascending order
+// ascending (a - b)
+const dogsSorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
+console.log(dogsSorted);
 
 /*
 
